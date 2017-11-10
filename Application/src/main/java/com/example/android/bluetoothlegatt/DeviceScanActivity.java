864239305847ecebde7e02031156b332e31b66fe
NCookies <swstar21c@gmail.com>
2017-11-10 11:00:11 +0900

    마시멜로우 권한 설정 및 연결 성공
    - 6.0(API 23) 버전부터는 블루투스 스캔을 위해서는 위치 권한이 필요함
    - 읽기/쓰기 기능을 사용하기 위해서 먼저 데이터가 어떤 방식으로 송수신되는지 알아봐야 함

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
@@ -27,8 +27,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -53,6 +55,7 @@ public class DeviceScanActivity extends ListActivity {
     private static final int REQUEST_ENABLE_BT = 1;
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
+    final private String TAG = "알림";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -117,37 +120,73 @@ public class DeviceScanActivity extends ListActivity {
 
         // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
         // fire an intent to display a dialog asking the user to grant permission to enable it.
-        if (!mBluetoothAdapter.isEnabled()) {
+//        if (!mBluetoothAdapter.isEnabled()) {
+//            if (!mBluetoothAdapter.isEnabled()) {
+//                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//            }
+//        }
+
+        // 권한설정 관련 코드
+        // 마시멜로우 호환 문제 때문에 아래 코드로 대체함
+        boolean isGranted = grantBluetoothPermission();
+
+        // BLE 세팅
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        if (isGranted) {
             if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
             }
+
+            // Initializes list view adapter.
+            mLeDeviceListAdapter = new LeDeviceListAdapter();
+            setListAdapter(mLeDeviceListAdapter);
+            scanLeDevice(true);
         }
-
-
-
-        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
-        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("This app needs location access");
-            builder.setMessage("Please grant location access so this app can detect peripherals.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
-                }
-            });
-            builder.show();
-        }
-
-        // Initializes list view adapter.
-        mLeDeviceListAdapter = new LeDeviceListAdapter();
-        setListAdapter(mLeDeviceListAdapter);
-        scanLeDevice(true);
     }
 
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    private boolean grantBluetoothPermission() {
+        // 마시멜로우 버전 이상일 때
+        if (Build.VERSION.SDK_INT >= 23) {
+            int accessCoarseLocation = checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+            int accessFineLocation   = checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION);
+            if (accessCoarseLocation == PackageManager.PERMISSION_GRANTED
+                    || accessFineLocation == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 이미 허가되어있을 때
+                return true;
+            } else {
+                // 권한이 없을 때
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) ||
+                        shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    // 사용자가 권한 요청을 거절했을 때 토스트 등을 띄어 사용자에게 필요성을 알려야 함
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                    return false;
+                } else {
+                    // 사용자가 권한을 요청한 적이 없음. 권한 요청 시도.
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                    return false;
+                }
+            }
+        } else {
+            // 마시멜로우 버전 이하일 때
+            return true;
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -158,10 +197,10 @@ public class DeviceScanActivity extends ListActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // 권한 허가
-// 해당 권한을 사용해서 작업을 진행할 수 있습니다
+                    // 해당 권한을 사용해서 작업을 진행할 수 있습니다
                 } else {
                     // 권한 거부
-// 사용자가 해당권한을 거부했을때 해주어야 할 동작을 수행합니다
+                    // 사용자가 해당권한을 거부했을때 해주어야 할 동작을 수행합니다
                 }
                 return;
         }
